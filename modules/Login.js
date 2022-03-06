291df82e7c94fe35ce4b82506622244c672c59cd
KGC9175 <g.chan273@gmail.com>
2022-03-07 00:00:46 +0900

    feat: Google 계정 로그인 기능 초기 설정

--- modules/Login.js
+++ modules/Login.js
@@ -1,1 +1,21 @@
 import { app } from '../fbase.js';
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  getAuth,
+} from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+const $btnLoginGoogle = document.querySelector('.link-google');
+
+$btnLoginGoogle.addEventListener('click', function (e) {
+  e.preventDefault();
+  signInWithPopup(auth, provider).then(async (result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    window.location.href = './pages/Main.html';
+  });
+});
