ed5b6073132ed779b50b2f927e560fbdd75ad588
KGC9175 <g.chan273@gmail.com>
2022-03-06 22:11:43 +0900

    feat: Firebase 초기설정

---
+++ fbase.js
@@ -0,0 +1,5 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
+
+import { firebaseConfig } from './apikey.js';
+
+export const app = initializeApp(firebaseConfig);
